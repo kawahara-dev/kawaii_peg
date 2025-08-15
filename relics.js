0e5091dc0b1b97cedee233dddf8165d8eed73d3d
kawahara-dev <kawahara.musiku@gmail.com>
2025-08-15 16:46:35 +0900

    Use PNG relic icons and rename assets

--- relics.js
+++ relics.js
@@ -5,31 +5,31 @@ export const relicList = [
     key: 'timeLag',
     name: 'タイムラグ',
     description: '1/5の確率で敵の攻撃を1ターン遅くする',
-    icon: 'image/relics/time_lag.svg'
+    icon: 'image/relics/TimeLag.png'
   },
   {
     key: 'rebound',
     name: 'リバウンド',
     description: '1/2の確率で落下したボールが再落下する',
-    icon: 'image/relics/rebound.svg'
+    icon: 'image/relics/Rebound.png'
   },
   {
     key: 'killHeal',
     name: 'キルヒール',
     description: '敵撃破でHP10回復',
-    icon: 'image/relics/kill_heal.svg'
+    icon: 'image/relics/KillHeal.png'
   },
   {
     key: 'damageBoost',
     name: 'ダメブースト',
     description: 'ペグ破壊時に追加ダメージ1~3',
-    icon: 'image/relics/damage_boost.svg'
+    icon: 'image/relics/DamageBoost.png'
   },
   {
     key: 'coinCharm',
     name: 'コインチャーム',
     description: '敵撃破でコイン10追加',
-    icon: 'image/relics/coin_charm.svg'
+    icon: 'image/relics/CoinCharm.png'
   }
 ];
 
