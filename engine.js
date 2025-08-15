acb440be5085a5be4e8445283979aee1ff048347
kawahara-dev <kawahara.musiku@gmail.com>
2025-08-15 22:54:10 +0900

    Run Matter render and runner loops

--- engine.js
+++ engine.js
@@ -36,6 +36,12 @@ function createGhostEngine() {
 }
 
 export function initEngine() {
+  if (render) {
+    Render.stop(render);
+  }
+  if (runner) {
+    Runner.stop(runner);
+  }
   engine = Engine.create();
   world = engine.world;
   render = Render.create({
@@ -43,9 +49,9 @@ export function initEngine() {
     engine,
     options: { width, height, wireframes: false, background: '#fff0f5' }
   });
-  // Render.run(render);
+  Render.run(render);
   runner = Runner.create();
-  // Runner.run(runner, engine);
+  Runner.run(runner, engine);
 
   Events.on(engine, 'beforeUpdate', () => {
     playerState.currentBalls.forEach(ball => {
